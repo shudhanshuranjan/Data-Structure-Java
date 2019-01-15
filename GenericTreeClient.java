bac35447e3a370429712450a48581b179ed9c4a7
Bhavya Jain <32821623+bhavya09@users.noreply.github.com>
2019-01-15 20:13:16 +0530

    Add files via upload

---
+++ GenericTreeClient.java
@@ -0,0 +1,17 @@
+package open;
+
+public class GenericTreeClient {
+
+	public static void main(String[] args) {
+		
+		GenericTree gt=new GenericTree();
+		System.out.println();
+	//	gt.display();
+        System.out.println();
+        System.out.println(gt.size());
+	}
+
+}
+
+
+//10 3 20 0 30 2 50 0 60 3 70 0 80 0 90 0 40 0
