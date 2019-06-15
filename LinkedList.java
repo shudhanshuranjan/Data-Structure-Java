40edc2f9f37a8da32bdb1fead7b061a71c6d36ec
Bhavya Jain <32821623+bhavya09@users.noreply.github.com>
2019-06-16 01:41:37 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,276 @@
+package LinkedList;
+
+
+
+public class LinkedList {
+	
+	private class Node
+	{
+		int data;
+		Node next;
+	}
+	
+	private Node head;
+	private Node tail;
+	int size;
+	
+	public void display()
+	{
+		System.out.println("------------------------------------");
+		Node temp=this.head;
+		while(temp!=null)
+		{
+			System.out.print(temp.data+ ", ");
+			temp=temp.next;
+		}
+		System.out.println();
+		System.out.println("------------------------------------");
+	}
+	
+	public void addLast(int item)
+	{
+		
+		Node nn=new Node();
+		nn.data=item;
+		nn.next=null;
+		
+		if(this.size>=1)
+		{
+			this.tail.next=nn;
+		}
+		
+		if(this.size==0)
+		{
+			this.head=nn;
+			this.tail=nn;
+			this.size++;
+		}
+		else
+		{
+			this.tail=nn;
+			this.size++;
+		}
+	}
+
+	public void addFirst(int item)
+	{
+		Node nn=new Node();
+		nn.data=item;
+		nn.next=null;
+		
+		if(this.size>=1)
+		{
+			nn.next=this.head;
+		}
+		if(this.size==0)
+		{
+			this.head=nn;
+			this.tail=nn;
+			this.size++;
+		}
+		else
+		{
+			this.head=nn;	
+			this.size++; 
+		}
+	}
+	
+    public int getFirst() throws Exception
+    {
+    	if(this.size==0)
+    	{
+    		throw new Exception("LL is Empty.");
+    	}
+    	return this.head.data;
+    }
+   
+    public int getLast() throws Exception
+    {
+    	if(this.size==0)
+    	{
+    		throw new Exception("LL is Empty.");
+    	}
+    	return this.tail.data;
+    }
+    
+    public int getAt(int idx) throws Exception
+    {
+        if(this.size==0)
+        {
+        	throw new Exception("LL is Empty");
+        }
+    	if(idx<0 || idx>=this.size)
+    	{
+    		throw new Exception("Invalid Index");
+    	}
+    	Node temp=this.head;
+    	for(int i=1;i<=idx;i++)
+    	{
+    		temp=temp.next;
+    	}
+    	return temp.data;
+    	
+    }
+    
+    private Node getNodeAt(int idx) throws Exception
+    {
+        if(this.size==0)
+        {
+        	throw new Exception("LL is Empty");
+        }
+    	if(idx<0 || idx>=this.size)
+    	{
+    		throw new Exception("Invalid Index");
+    	}
+    	Node temp=this.head;
+    	for(int i=1;i<=idx;i++)
+    	{
+    		temp=temp.next;
+    	}
+    	return temp;
+    }
+    
+    public void addAt(int idx,int item) throws Exception
+    {
+    
+      	if(idx<0 || idx>this.size)
+      	{
+      		throw new Exception("Invalid Index");
+      	}
+      	if(idx==0)
+      	{
+      		addFirst(item);
+      	}
+      	else if(idx==this.size)
+      	{
+      		addLast(item);
+      	}
+      	else
+      	{    	
+      	Node nn=new Node();
+      	nn.data=item;
+      	nn.next=null;
+    	Node nm1=getNodeAt(idx-1);
+    //	System.out.println(nm1.data);
+    	Node np1=nm1.next;
+        nm1.next=nn;
+        nn.next=np1;
+        this.size++;
+      	}
+    }
+
+    public int removeFirst() throws Exception
+    {
+    	if(this.size==0)
+    	{
+    		throw new Exception("LL is Empty");
+    	}
+    	int rv=head.data;
+    	if(this.size==1)
+    	{
+    		this.head=null;
+    		this.tail=null;
+    		this.size=0;
+    	}
+    	else
+    	{
+    		this.head=this.head.next;
+    		this.size--;  
+    	}
+    	return rv;
+    }
+
+    public int removeLast() throws Exception
+    {
+    	if(this.size==0)
+    	{
+    		throw new Exception("LL is Empty");
+    	}
+    	int rv=tail.data;
+    	
+    	if(this.size==1)
+    	{
+    		this.head=null;
+    		this.tail=null;
+    		this.size=0;
+    	}
+    	else
+    	{
+    		Node lm=this.getNodeAt(this.size-2);
+    		this.tail=lm;
+    		this.tail.next=null;
+    		this.size--;
+    	}
+    	
+    	return rv;
+    }
+
+    public int removeAt(int idx) throws Exception
+    {
+    	if(this.size==0)
+    	{
+    		throw new Exception("LL is Empty.");
+    	}
+    	if(idx < 0 || idx >= this.size)
+    	{
+    		throw new Exception("Invalid Index");
+    	}
+    	if(idx==0)
+    	{
+    		return removeFirst();
+    	}
+    	else if(idx==this.size)
+    	{
+    		return removeLast();
+    	}
+    	else
+    	{
+    		Node nm1=getNodeAt(idx-1);
+    		Node n=nm1.next;
+    		Node np1=n.next;
+    		nm1.next=np1;
+    		this.size--;
+    		return n.data;
+    	}	
+    }
+    
+    public void reverseData() throws Exception
+    {
+    	int left=0;
+    	int right=this.size-1;
+    	
+    	while(left<right)
+    	{
+    		Node le=getNodeAt(left);
+    		Node ri=getNodeAt(right);
+    		
+    		int temp=le.data;
+    		le.data=ri.data;
+    		ri.data=temp;
+    		left++;
+    		right--;
+    		
+    	}
+    }
+    
+    public void reversePointers()
+    {
+    	Node prev=this.head;
+    	Node curr=prev.next;
+    	
+    	while(curr!=null)
+    	{
+    		Node ahead=curr.next;
+    		curr.next=prev;
+    		prev=curr;
+    		curr=ahead;
+    	}
+    	
+    	Node t=this.head;
+    	this.head=this.tail;
+    	this.tail=t;
+    	this.tail.next=null;
+    }
+
+}
+
